b730745323edb4b0f50b1ef6114728ed36f55ada
Léo Bouard <leo.bouard@hotmail.com>
2022-11-08 10:53:59 +0100

    Publication initiale
    Fonctionnalités initiales

---
+++ DynamicADGroups.ps1
@@ -0,0 +1,265 @@
+#Requires -Version 5.1
+#Requires -Modules @{ModuleName="ActiveDirectory";ModuleVersion="1.0.1.0"}
+
+[CmdletBinding(SupportsShouldProcess=$True)]
+Param(
+    [Parameter(Mandatory=$True)][System.IO.FileInfo]$JsonFile,
+    [Parameter(Mandatory=$True)][System.Object[]]$ReportTo,
+    [System.String]$DefaultSearchBase = (Get-ADDomain).DistinguishedName,
+    [System.String]$TestRecipient,
+    [System.String]$SmtpServer = "smtp.contoso.com",
+    [System.String]$FromAddress = "noreply@contoso.com"
+)
+
+begin {
+
+    $logPath = "$PSScriptRoot\logs\DynamicADGroups_$(Get-Date -Format 'yyyy-MM-dd-HHmmss').txt"
+    Start-Transcript -Path $logPath
+    $PSDefaultParameterValues = @{
+        "Import-Module:Verbose"            = $false
+        "Remove-Variable:WhatIf"           = $false
+        "Remove-Variable:ErrorAction"      = "SilentlyContinue"
+        "Get-ADGroup*:ErrorAction"         = "Continue"
+        "Add-ADGroupMember:ErrorAction"    = "Stop"
+        "Remove-ADGroupMember:ErrorAction" = "Stop"
+    }
+    
+    # Get all users from Active Directory
+    Import-Module ActiveDirectory
+    try {
+        Write-Verbose -Message "Get all users from search base: '$DefaultSearchBase'"
+        $allUsers = Get-ADUser -Filter * -Properties * -SearchBase $DefaultSearchBase
+        Write-Verbose -Message "Accounts found: $(($allUsers | Measure-Object).Count)"
+    } catch {
+        Write-Error -Exception "$($_.Exception.Message)"
+        exit
+    }
+
+    # Get data from JSON file
+    Write-Verbose -Message "Getting data from JSON file: $JsonFile"
+    $data = Get-Content -Path $JsonFile -Encoding UTF8
+    Write-Verbose -Message "Convert data from JSON"
+    $data = $data | ConvertFrom-Json
+
+    $finalReport    = [System.Collections.Generic.List[PSCustomObject]]@()
+    $groupsNotFound = [System.Collections.Generic.List[PSCustomObject]]@()
+}
+
+process {
+
+    foreach ($_ in $data) {
+
+        # Get Active Directory group
+        $groupName = $_.Group
+        Write-Verbose -Message "Getting Active Directory group '$($_.Group)'"
+        $group = Get-ADGroup -Filter {Name -eq $groupName}
+        if ($null -eq $group) { 
+            Write-Error -Exception "Couldn't found group '$($_.Group)' in Active Directory!"
+            $groupsNotFound.Add($_)
+        } else {
+
+            # Get Active Directory group members
+            Write-Verbose -Message "Getting group members (direct membership only)"
+            $groupMembers = Get-ADGroupMember -Identity $group | Where-Object {$_.objectClass -eq "user"}
+
+            # Get all users on searchbase
+            if ($_.SearchBase) { 
+                if ($_.SearchBase -notlike "OU=*") { $searchBase = 'OU=' + $_.SearchBase + ',' + $DefaultSearchBase }
+            } else { $searchBase = $DefaultSearchBase }
+            $users = $allUsers | Where-Object {$_.DistinguishedName -match $searchBase}
+
+            # Filter out using presets
+            switch ($_.Preset) {
+                "Messaging"    { $users = $users | Where-Object {$_.Enabled -eq $true -and $_.msExchRecipientTypeDetails -eq 2147483648} }
+                "EnabledOnly"  { $users = $users | Where-Object {$_.Enabled -eq $true} }
+                "DisabledOnly" { $users = $users | Where-Object {$_.Enabled -eq $false} }
+            }
+
+            # Filter out using the custom filter
+            if ($_.Filter) {
+                $filter = $_.Filter
+                $users = $users | Where-Object {Invoke-Expression $filter}
+                Remove-Variable filter -ErrorAction SilentlyContinue
+            }
+            
+            # Compare filter results vs. current group members
+            $compare = [System.Collections.Generic.List[PSCustomObject]]@()
+
+                # 1. The group is empty
+                if ($null -eq $groupMembers) {
+                    $users | ForEach-Object {
+                        $compare.Add([PSCustomObject]@{
+                            Identity     = $_.SamAccountName
+                            Name         = $_.Name
+                            Company      = $_.Company
+                            Title        = $_.Title
+                            Department   = $_.Department
+                            EmailAddress = $_.EmailAddress
+                            Action       = "Add"
+                        })
+                    }
+                }
+
+                # 2. Everyone in the group should be removed
+                elseif ($null -eq $users) {
+                    $groupMembers | ForEach-Object {
+                        $sam    = $_.SamAccountName
+                        $member = $allUsers | Where-Object {$_.SamAccountName -eq $sam}
+                        $compare.Add([PSCustomObject]@{
+                            Identity     = $sam
+                            Name         = $member.Name
+                            Company       = $member.Company
+                            Title        = $member.Title
+                            Department   = $member.Department
+                            EmailAddress = $member.EmailAddress
+                            Action       = "Remove"
+                        })
+                        Remove-Variable sam,member -ErrorAction SilentlyContinue
+                    }
+                }
+
+                # 3. Regular case (add & remove members)
+                else {
+                    Compare-Object -ReferenceObject $users.SamAccountName -DifferenceObject $groupMembers.SamAccountName -IncludeEqual | Foreach-Object { 
+                        $sam    = $_.InputObject
+                        $member = $allUsers | Where-Object {$_.SamAccountName -eq $sam}
+                        switch ($_.SideIndicator) {
+                            "==" { $action = "Keep" }
+                            "=>" { $action = "Remove" }
+                            "<=" { $action = "Add" }
+                        }
+                        $compare.Add([PSCustomObject]@{
+                            Identity     = $_.InputObject
+                            Name         = $member.Name
+                            Company      = $member.Company
+                            Title        = $member.Title
+                            Department   = $member.Department
+                            EmailAddress = $member.EmailAddress
+                            Action       = $action
+                        })
+                        Remove-Variable sam,member,action -ErrorAction SilentlyContinue
+                    }
+                }
+
+            # Filter out exceptions
+            if ($null -ne $_.Exceptions) {
+                $exceptions = $_.Exceptions
+                $compare | Where-Object {$_.Identity -in $exceptions -and $_.Action -eq "Remove"} | Foreach-Object {$_.Action = "Keep"}
+                Remove-Variable exceptions -ErrorAction SilentlyContinue
+            }
+
+            Write-Output -InputObject "Processing group $($_.Group)"
+            Write-Output -InputObject ($compare | Format-Table)
+
+            # Add & remove group members
+            $compare | Where-Object {$_.Action -ne "Keep"} | ForEach-Object {
+                # Add members
+                if ($_.Action -eq "Add") {
+                    Write-Verbose -Message "Add $($_.Identity) to group $($group.Name)"
+                    try { 
+                        Add-ADGroupMember -Identity $group -Members $_.Identity -Confirm:$false -WhatIf:$WhatIf
+                        $colorHex = "008000"
+                        $text     = "Added"
+                    } catch {
+                        Write-Error -Exception "$($_.Exception.Message)"
+                        $colorHex = "ff0000"
+                        $text     = "Error! Couldn't add user to group"
+                    }
+                }
+                # Remove members
+                if ($_.Action -eq "Remove") {
+                    Write-Verbose -Message "Remove $($_.Identity) from group $($group.Name)"
+                    try {
+                        Remove-ADGroupMember -Identity $group -Members $_.Identity -Confirm:$false -WhatIf:$WhatIf
+                        $colorHex = "518EC9"
+                        $text     = "Removed"
+                    } catch {
+                        Write-Error -Exception "$($_.Exception.Message)"
+                        $colorHex = "ff0000"
+                        $text = "Error! Couldn't remove user from group"
+                    }
+                }
+
+                $_ | Add-Member -MemberType NoteProperty -Name Status -Value "<font color=$colorHex>$text</font>" -Force
+                Remove-Variable colorHex,text -ErrorAction SilentlyContinue
+            }
+
+            $compare = $compare | Select-Object @{N="Group";E={$groupName}},Name,Title,Department,EmailAddress,Status
+            $count = ($compare | Where-Object {$_.Status} | Measure-Object).Count
+
+            # Send group report
+            if ($_.ReportTo -and $count -gt 0) {
+
+                # Create mail body
+                $content = @"
+<h2>$groupName</h2>
+<h3>Modified members</h3>
+$([System.String]($compare | Where-Object {$_.Status} | ConvertTo-Html -Fragment))
+<h3>Members retained</h3>
+$([System.String]($compare | Where-Object {!$_.Status} | Select-Object Group,Identity,@{N="Status";E={"Kept"}} | ConvertTo-Html -Fragment))
+"@
+                $content = $content -replace "&lt;","<"
+                $content = $content -replace "&gt;",">"
+                $body    = [System.String](Get-Content -Path "$PSScriptRoot\body.html" -Encoding UTF8)
+                $body    = $body -replace "{{ content }}",$content
+
+                # Send mail message
+                $mailParams = @{
+                    Body       = $body
+                    BodyAsHtml = $True
+                    Encoding   = "UTF8"
+                    From       = $FromAddress
+                    Subject    = "Updates to group $groupName"
+                    SmtpServer = $SmtpServer
+                    To         = $_.ReportTo
+                }
+                if ($TestRecipient) { $mailParams.To = $TestRecipient }
+                Write-Verbose -Message "Send group report to $([System.String]($mailParams.To))"
+                Send-MailMessage @mailParams
+
+                Remove-Variable content,body,mailParams -ErrorAction SilentlyContinue
+            }
+
+            $compare | Where-Object {$_.Status} | ForEach-Object { $finalReport.Add($_) }
+        }
+    }
+}
+
+end {
+
+    # Create mail body
+    $content = "<h2>Dynamic groups updates</h2><p>The synthesis of the processing of the JSON file by the PowerShell script 'DynamicADGroup.ps1' for the automatic populating of Active Directory groups</p>"
+    if ($groupsNotFound.Count -ne 0) { 
+        $content += "<h3>Groups not found</h3><p>Here's the list of groups that couldn't be found in Active Directory using the provided name in the JSON file:<ul>"
+        $groupsNotFound.Group | ForEach-Object { $content += "<li>$_</li>" }
+        $content += "</ul></p>"
+    }
+    ($finalReport | Sort-Object Group).Group | Get-Unique | Foreach-Object {
+        $groupName = $_
+        $content += "<h3>$groupName</h3>"
+        $content += [System.String]($finalReport | Where-Object {$_.Group -eq $groupName} | ConvertTo-Html -Fragment)
+        Remove-Variable groupName,description -ErrorAction SilentlyContinue
+    }
+    $content = $content -replace "&lt;","<"
+    $content = $content -replace "&gt;",">"
+    $body = [System.String](Get-Content -Path "$PSScriptRoot\body.html" -Encoding UTF8)
+    $body = $body -replace "{{ content }}",$content
+
+    # Send mail message
+    $mailParams = @{
+        Body        = $body
+        BodyAsHtml  = $True
+        Encoding    = "UTF8"
+        From        = $FromAddress
+        Subject     = "DynamicADGroups $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+        SmtpServer  = $SmtpServer
+        To          = $ReportTo
+    }
+    if ((Test-Path -Path $logPath) -eq $true) { $mailParams.Attachments = Get-Item -Path $logPath }
+    if ($TestRecipient) { $mailParams.To = $TestRecipient }
+    Write-Verbose -Message "Send final report to $([System.String]($mailParams.To))"
+    Send-MailMessage @mailParams
+
+    Stop-Transcript
+    
+}
---
+++ README.md
@@ -0,0 +1,157 @@
+# Groupes Active Directory dynamiques
+
+## Possibilités
+
+Vous pouvez alimenter automatiquement des groupes Active Directory en fonction de règles d'appartenances qui se basent sur les valeurs des attributs Active Directory des comptes utilisateurs.
+
+Dans 90% des cas, les règles d'appartenances vont se limiter aux attributs suivants :
+
+- Department : le service dans lequel l'utilisateur travaille
+- Title : le nom du poste qu'occupe l'utilisateur
+- Office : l'emplacement du bureau de l'utilisateur
+
+Cependant, il est possible de requêter sur tous les attributs Active Directory des comptes utilisateurs (extensionAttribute, accountExpirationDate...)
+
+### Limitations
+
+Quelques limitations sont a connaitre :
+
+1. Ne convient pas aux groupes contenant plus de 5000 membres directs (limitation liée à PowerShell)
+2. Ne met à jour que les membres directs des groupes
+3. Ne doit pas être utilisé pour peupler automatiquement des groupes à privilèges (pour des raisons de sécurité)
+
+### Compatibilité
+
+Ce script est compatible avec les groupes existants, qu'importe leur type (domaine local, sécurité globale, sécurité de distribution, universel...). 
+
+## Fonctionnement
+
+### Résumé
+
+Le script utilise des fichiers JSON pour peupler automatiquement les groupes demandés. Vous pouvez regrouper plusieurs groupes dynamiques dans un seul fichier JSON.
+
+Propriété   | Description             | Type                  | Exemple
+----------- | ----------------------- | --------------------- | -------
+**Group**   | Nom du groupe à peupler | Chaine de caractères  | GG-CONTOSO-ITTEAM
+Preset      | Réglage pré-défini      | Chaine de caractères  | EnabledOnly<br>Messaging
+**Filter**  | Règle d'appartenance    | Bloc de script        | `$_.Title -in 'IT Technician','IT Manager'`
+SearchBase  | Périmètre de recherche  | Chaine de caractères  | CONTOSO
+Exceptions  | Exceptions              | Chaine multi-valeurs  | jack.black<br>john.smith
+ReportTo    | Contacts à informer     | Chaine multi-valeurs  | <jblack@contoso.com><br><jsmith@contoso.com>
+
+Les propriétés obligatoires sont indiquées en gras.
+
+### Réglages pré-définis
+
+Nom | Description
+--- | -----------
+Messaging | Ne garde que les comptes Active Directory actif avec une boite aux lettres active (RemoteMailbox)
+EnabledOnly | Ne garde que les comptes Active Directory actif (Enabled = True)
+DisabledOnly | Ne garde que les comptes Active Directory désactivés (Enabled = False)
+
+Si vous ne spécifiez pas de réglage, aucun filtre ne sera fait avant la règle d'appartenance au groupe.
+
+### Règle d'appartenance
+
+Le filtre sera interprété dans une commande "Where-Object" et peut donc contenir :
+
+- tous les opérateurs de comparaison (-LT, -GT, -EQ, -CONTAINS, -LIKE, -IN, -MATCH...)
+- plusieurs conditions jointes via les opérateurs "AND" et "OR"
+- gestion des priorités dans les comparaisons avec des parenthèses
+
+Les attributs doivent être utilisés avec la syntaxe PowerShell plutôt que LDAP.
+
+Lien vers la documentation : [Where-Object | Microsoft Learn](https://learn.microsoft.com/fr-fr/powershell/module/microsoft.powershell.core/where-object)
+
+### Périmètre de recherche
+
+Le périmètre de recherche permet de limiter la règle d'appartenance à une seule unité d'organisation (les unités d'organisation enfants font partie du périmètre).
+
+Deux formes de périmètre de recherche sont acceptés :
+
+- avec le nom de l'unité d'organisation directement sous le périmètre par défaut : CONTOSO
+- le DistinguishedName complet vers l'unité d'organisation : OU=CONTOSO,DC=corp,DC=contoso,DC=com
+
+Si aucun périmètre de recherche n'est spécifié, c'est le périmètre par défaut qui sera utilisé.
+
+### Exceptions
+
+Les exceptions permettent de conserver des membres dans un groupe, même s'ils ne correspondent pas aux règles d'appartenance de celui-ci. Les exceptions doivent être indiquées avec le SamAccountName de chaque compte, dans un objet JSON avec plusieurs valeurs.
+
+### Contacts à informer
+
+Des adresses emails peuvent être indiquées pour chaque groupe afin d'informer des mouvements (entrées et sorties) des membres du groupe. 
+
+<div style='background-color: #efd77d; padding-left: 25px; padding-right: 25px; padding-top: 10px; padding-bottom: 10px; margin: 5px;'>
+  <p><b>⚠️ Attention :</b> cette propriété JSON ne doit pas être confondue avec le paramètre général du script PowerShell qui permet d'avoir une revue complète de tous les mouvements sur tous les groupes du fichier JSON</p>
+</div>
+
+### Traduction en JSON
+
+```json
+[
+    {
+        "Group":  "GG-CONTOSO-ITTEAM",
+        "Preset":  "EnabledOnly",
+        "Filter":  "$_.Title -in 'IT Technician','IT Manager'",
+        "SearchBase":  "CONTOSO",
+        "Exceptions":  [
+            "jack.black",
+            "john.smith"
+        ]
+    },
+    {
+        "Group":  "GG-FABRIKAM-SALESMGMT",
+        "Filter":  "$_.Department -eq 'Sales' -and $_.Title -like 'Manager*'",
+        "SearchBase":  "FABRIKAM",
+        "ReportTo":  "jsmith@contoso.com"
+    }
+]
+```
+
+## Mise en tâche planifiée
+
+### Paramètres du script
+
+Paramètre | Valeur par défaut | Description
+--------- | ----------------- | -----------
+**JsonFile** | | Chemin vers le fichier JSON à traiter
+**ReportTo** | | Destinataire(s) pour reçevoir le rapport complet sur les actions effectuées par le script
+DefaultSearchBase | Racine du domaine | Périmètre de recherche des utilisateurs
+TestRecipient | | Destinataire pour toutes les communications en cas de test (les actions d'ajout et suppression seront quand-même effectuées)
+SmtpServer | smtp.contoso.com | Serveur de messagerie à utiliser
+FromAddress | <noreply@contoso.com> | Adresse d'expédition
+
+Les paramètres obligatoires sont indiqués en gras.
+
+Le script est également compatible avec les paramètres génériques comme `-Verbose` et `-WhatIf`.
+
+### Mise en tâche planifiée
+
+Vous pouvez créer une ou plusieurs tâche(s) planifiée pour mettre à jour les groupes. Vous pouvez utiliser un compte de service type GMSA (Group Managed Service Accounts) avec suffisamment de permissions dans l'Active Directory pour :
+
+- lire les informations des comptes utilisateurs
+- ajouter et supprimer les membres d'un groupe
+- envoyer les emails de rapports
+
+Il est recommandé d'utiliser un fichier JSON différent pour chaque tâche planifiée.
+
+### Exemple d'utilisation
+
+Voici un exemple de paramètres pour lancer le script PowerShell :
+
+```powershell
+.\DynamicADGroups.ps1 -JsonFile ".\data\TESTING.json" `
+    -ReportTo "jsmith@contoso.com" `
+    -DefaultSearchBase "OU=CONTOSO,DC=corp,DC=contoso,DC=com" `
+    -SmtpServer "smtp.contoso.com" `
+    -FromAddress "noreply@contoso.com" `
+    -TestRecipient "jsmith@contoso.com" `
+    -WhatIf -Verbose
+```
+
+### Message de synthèse
+
+Après une exécution, le ou les destinataire(s) indiqué(s) dans le paramètre `-ReportTo` du script reçoivent une synthèse de toutes les modifications apportées aux groupes :
+
+![capture-resultat-synchro](data/capture01.png)
